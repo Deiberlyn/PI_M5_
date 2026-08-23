3a52cfe733373c8f22c4965dc275e8888a5069c6
Deiberlyn <deiberlyn.3@gmail.com>
2026-08-22 23:18:35 -0400

    feat: implementamos funcion de cargarDatos en cargar_datos.py

---
+++ src/cargar_datos.py
@@ -0,0 +1,30 @@
+# Importe de Librerias
+
+import os
+import pandas as pd
+
+# Se genera la función cargarDatos
+def cargarDatos():
+    # 1. Ruta absoluta del directorio src
+    ruta_actual = os.path.dirname(os.path.abspath(__file__))
+
+    # 2. Subir un nivel hacia la carpeta raíz PI_M5_
+    ruta_proyecto = os.path.dirname(ruta_actual)
+
+    # 3. Construir la ruta completa al archivo Excel
+    ruta_excel = os.path.join(ruta_proyecto, "Base_de_datos.xlsx")
+
+    # 4. Leer los datos
+    df = pd.read_excel(ruta_excel)
+    print(df)
+    return df
+
+
+if __name__ == "__main__":
+    datos = cargarDatos()
+
+    print("\n--- PRIMERAS FILAS ---")
+    print(datos.head())
+
+    print("\n--- COLUMNAS ENCONTRADAS ---")
+    print(datos.columns)
